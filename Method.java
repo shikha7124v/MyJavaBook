247f91c374bbb4d5cd9b6f9fa742afafbc2c29de
shikha7124 <shikhaverma7124@gmail.com>
2024-01-21 10:23:30 +0530

    java method

---
+++ Method.java
@@ -0,0 +1,9 @@
+public class Method {
+    static void myMethod(String fname) {
+        System.out.println(fname + "Shikha");
+    }
+    public static void main(String[] args) {
+        myMethod("Verma");
+        myMethod("Shikha");
+    }
+}
