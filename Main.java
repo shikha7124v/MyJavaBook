69f0a61d4250dc2c22fc78829639a92e713816a9
shikha7124 <shikhaverma7124@gmail.com>
2024-01-21 09:57:41 +0530

    java method

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static void myMethod() {
+        System.out.println("It's a method !!");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
