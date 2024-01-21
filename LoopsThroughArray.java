5497c64be421f79e6e051a5a5b8c44c275949d8e
shikha7124 <shikhaverma7124@gmail.com>
2024-01-21 11:22:16 +0530

    Loops Through Array

---
+++ LoopsThroughArray.java
@@ -0,0 +1,8 @@
+public class LoopsThroughArray {
+    public static void main(String[] args) {
+        String[] vowel = {"a","e","i","o","u"};
+        for(String i: vowel) {
+            System.out.println(i);
+        }
+    }
+}
