9944c36dd2f56909e7437513ff66d9c3d2814e68
shikha7124 <shikhaverma7124@gmail.com>
2024-01-21 10:49:44 +0530

    Math

---
+++ MathJava.java
@@ -0,0 +1,11 @@
+public class MathJava {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,10));
+        System.out.println(Math.min(1,10));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-7.8));
+        System.out.println(Math.random());
+        int randomNum = (int)(Math.random() * 101);
+        System.out.println(randomNum);
+    }
+}
