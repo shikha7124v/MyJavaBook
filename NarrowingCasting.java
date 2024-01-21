3fa05e514a5f5aea08cdaddbdeae105492048808
shikha7124 <shikhaverma7124@gmail.com>
2024-01-21 10:45:01 +0530

    Narrowing Casting

---
+++ NarrowingCasting.java
@@ -0,0 +1,9 @@
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myInt = (int)myDouble;
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
