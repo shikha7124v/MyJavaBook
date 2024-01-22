36b595fe577efb61917e3b518ba0555285618663
shikha7124 <shikhaverma7124@gmail.com>
2024-01-22 11:55:24 +0530

    class and object

---
+++ Java-Class/JavaClass.java
@@ -0,0 +1,10 @@
+public class JavaClass {
+    int x = 5;
+    public static void main(String[] args) {
+        JavaClass obj1 = new JavaClass();
+        JavaClass obj2 = new JavaClass();
+
+        System.out.println(obj1.x);
+        System.out.println(obj2.x);
+    }
+}
