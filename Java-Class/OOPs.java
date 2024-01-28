dd5fe250efd32464f1471edf109288956fc16643
shikha7124 <shikhaverma7124@gmail.com>
2024-01-28 18:15:16 +0530

    OOP

--- Java-Class/OOPs.java
+++ Java-Class/OOPs.java
@@ -1,1 +1,10 @@
-public 
+public class OOPs {
+    String fname = "Shikha";
+    String lname = "Verma";
+    int age = 21;
+
+    public static void main(String[] args) {
+        OOPs obj = new OOPs();
+        System.out.println(obj.fname + " " + obj.lname + " is " + obj.age + " years old.");
+    }
+}
