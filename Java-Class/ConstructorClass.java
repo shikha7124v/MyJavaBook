927cec520ec395fcdbff6ed9044611b505e5bdcc
shikha7124 <shikhaverma7124@gmail.com>
2024-01-28 18:28:40 +0530

    constructor

--- Java-Class/ConstructorClass.java
+++ Java-Class/ConstructorClass.java
@@ -3,8 +3,14 @@ public class ConstructorClass {
     public ConstructorClass() {
         x = 18;
     }
+    //Parameterised Constructor
+    public ConstructorClass(int y) {
+        x = y;
+    }
     public static void main(String[] args) {
         ConstructorClass obj = new ConstructorClass();
         System.out.println(obj.x);
+        ConstructorClass obj1 = new ConstructorClass(5);
+        System.out.println(obj1.x);
     }
 }
