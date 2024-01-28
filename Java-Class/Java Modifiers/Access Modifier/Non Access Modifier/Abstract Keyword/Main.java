efa6fa3aa5d69078272cc723d1053349b73a4d12
shikha7124 <shikhaverma7124@gmail.com>
2024-01-28 19:45:06 +0530

    abstract keyword

---
+++ Java-Class/Java Modifiers/Access Modifier/Non Access Modifier/Abstract Keyword/Main.java
@@ -0,0 +1,6 @@
+abstract class Main {
+    public String fname = "Shikha";
+    public String lname = "Verma";
+    public String emain = "shikhaverma.wit@outlook.com";
+    public abstract void study();
+}
---
+++ Java-Class/Java Modifiers/Access Modifier/Non Access Modifier/Abstract Keyword/Student.java
@@ -0,0 +1,8 @@
+class Student extends Main {
+    public static void main(String[] args) {
+    public int year = 2024;
+        public void study() {
+            System.out.println("Studying all day long");
+        }
+    }
+}
