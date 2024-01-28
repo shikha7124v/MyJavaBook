03c6189df7ca0afb7f97afc5629fe8b890a86446
shikha7124 <shikhaverma7124@gmail.com>
2024-01-28 19:28:52 +0530

    access modifier

---
+++ Java-Class/Java Modifiers/Main.java
@@ -0,0 +1,7 @@
+class Main extends Person {
+    private int Year = 2024;
+    public static void main(String[] args) {
+        Main obj = new Main();
+        System.out.println("Name : "+obj.fname+" "+obj.lname);
+    }
+} 
